7a2209e9490c2576a01f3c262619b725d3357bb7
AhmedAlfahal <ahmednader02@gmail.com>
2023-04-23 11:09:46 +0400

    header file added.

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: aalfahal <aalfahal@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 02:08:40 by aalfahal          #+#    #+#             */
-/*   Updated: 2023/04/23 10:54:04 by aalfahal         ###   ########.fr       */
+/*   Updated: 2023/04/23 11:08:59 by aalfahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,24 @@
 # include"libft/libft.h"
 # include"ft_printf/ft_printf.h"
 # include"get_next_line/get_next_line.h"
-# include "mlx/mlx.h"
+# include"mlx/mlx.h"
 
-# include<stdio.h>
+typedef enum color_type
+{
+	r,
+	g,
+	b,
+}	t_color_type;
 
 typedef struct s_cub3d
+{
+	char	**map;
+	char	*no_text;
+	char	*so_text;
+	char	*we_text;
+	char	*ea_text;
+	int		*f_rgb;
+	int		*c_rgb;
+}			t_cub3d;
+
 #endif
